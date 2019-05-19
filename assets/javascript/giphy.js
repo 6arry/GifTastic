321fc6e2509a6cee761366e226e69d550d42538a
gccunanan <gccunanan@gmail.com>
2019-05-18 20:47:19 -0700

    WELL TIME TO FINISH HW: BLAZERS LOST

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -18,7 +18,25 @@ function populateButtons(searchArray, classToAdd, areaToAddTo){
 
 $(document).on("click", ".searchButton", function(){
     var type = $(this).data("type");
-    
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=UNUrXOwdD5LUj9OXrpezgJTnU1ytlcTJ&limit=10&q=" + type;
+    $.ajax({url: queryURL, method: "GET"}).done(function(response){
+        for (var i=0; i<response.data.length;i++){
+            var searchDiv = $("<div class='search-item'>");
+            var rating = response.data[i].rating;
+            var p = $('<p>').text("Rating: " + rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $("<img>");
+            image.attr("src", still);
+            image.attr("data-still", still);
+            image.attr("data-animated", animated);
+            image.attr("data-state", "still");
+            image.addClass("searchImage");
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $("#searchs").append(searchDiv);
+        }
+    })
     // console.log(type);
 })
 
